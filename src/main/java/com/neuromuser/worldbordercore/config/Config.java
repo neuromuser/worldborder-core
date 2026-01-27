39727e1e24148ed4d38ea9f04217ceb3ffad37cf
neuromuser <neuromuser@gmail.com>
2026-01-27 20:37:01 +0200

    1.6.1 update

--- src/main/java/com/neuromuser/worldbordercore/config/Config.java
+++ src/main/java/com/neuromuser/worldbordercore/config/Config.java
@@ -4,4 +4,9 @@ public class Config {
     public double nonRenewableMultiplier = 0.3;
     public double progressionMultiplier = 1.1;
     public double randomnessVariation = 0.1;
+    public double borderIncreaseAmount = 10.0;
+    public double diamondRewardBaseChance = 0.1;
+    public int diamondRewardMinAmount = 1;
+    public int diamondRewardMaxAmount = 4;
+    public double diamondRewardChanceIncreasePerLevel = 0.01;
 }
